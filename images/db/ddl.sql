2c382e2b16cd7eefad124d8ec303494a19bf38a1
=isiin <=isiin@abeam.ocn.ne.jp>
2022-06-24 07:52:39 +0900

    test

--- images/db/ddl.sql
+++ images/db/ddl.sql
@@ -2,11 +2,12 @@ DROP SCHEMA IF EXISTS testdb;
 CREATE SCHEMA testdb;
 USE testdb;
 
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS account
 (
   id           INT(10) NOT NULL AUTO_INCREMENT,
   name     VARCHAR(40)
+  email     VARCHAR(40)
 );
 
-INSERT INTO user (name) VALUES ("user1");
-INSERT INTO user (name) VALUES ("user2");
+INSERT INTO account (name, email) VALUES ("user1", "user1@example.com");
+INSERT INTO account (name, email) VALUES ("user2", "user2@example.com");

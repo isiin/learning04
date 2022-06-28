54d55dd75b3ab94b5a61e24775551fc621146f9c
=isiin <=isiin@abeam.ocn.ne.jp>
2022-06-28 13:39:18 +0900

    -

--- images/db/initdb.d/ddl.sql
+++ images/db/initdb.d/ddl.sql
@@ -1,3 +1,6 @@
+CREATE ROLE dbuser WITH LOGIN PASSWORD 'dbpass';
+CREATE SCHEMA IF NOT EXISTS test AUTHORIZATION dbuser;
+
 CREATE TABLE account(
   id SERIAL NOT NULL, 
   name VARCHAR(20) NOT NULL, 
@@ -8,3 +11,4 @@ CREATE TABLE account(
 
 INSERT INTO account (name, email) VALUES ('user1', 'user1@example.com');
 INSERT INTO account (name, email) VALUES ('user2', 'user2@example.com');
+
